091eda60a32dc436672cf5e863e68efab6220416
Sriraam Thiruchelvam <sriraamthiruchelvam@gmail.com>
2021-10-22 23:50:22 -0400

    Tutorial5

--- scripts.js
+++ scripts.js
@@ -1,8 +1,15 @@
-let nextPlayer = 'X'; // takes a value of either 'X' or 'O' according to the game turns
+let nextPlayer = 'X'
+// takes a value of either 'X' or 'O' according to the game turns
 
-//initialize the game
+let ticTacToe = document.getElementsByTagName("td");//initialize the game
 
 // use the value stored in the nextPlayer variable to indicate who the next player is
+let player1 = "X";
+let player2 = "O";
+let label = document.getElementById("next-lbl");
+label.innerHTML() = nextPlayer;
+
+
 
 
 //This call will create the buttons needed for the gameboard.
@@ -10,11 +17,15 @@ createGameBoard()
 
 function createGameBoard()
 {
-    // Programatically add a button with square brackets enclosing an empty space to each cell in the gameboard
-   
+    
+    let btns = document.querySelectorAll("button");// Programatically add a button with square brackets enclosing an empty space to each cell in the gameboard
+    for(let i=0;i<9;i++){
+        btns[i].innerText = "[]";
+    
+    }
 }
 
-// Programatically add 'takeCell' as an event listener to all the buttons on the board
+btns.addEventListener(takeCell);// Programatically add 'takeCell' as an event listener to all the buttons on the board
 let btns = document.querySelectorAll('button');
 for (let i=0; i<btns.length; i++)
 {
@@ -22,19 +33,33 @@ for (let i=0; i<btns.length; i++)
 }
 
 // This function will be used to respond to a click event on any of the board buttons.
+let player1;
+let player2;
 function takeCell(event)
 {
     /*
         When the button is clicked, the space inside its square brackets is replaced by the value in the nextPlayer before switching it
     */
+   if(nextPlayer == "X"){
+     player1 = "X"
+}else{
+    player2 = "O"
+   }
+
+
+   
 
     // Make sure the button is clickable only once (I didn't mention how to do that, look it up :) )
 
     // Check if the game is over
-    if (isGameOver())
-    {
-        // let the lable with the id 'game-over-lbl' display the words 'Game Over' inside <h1> element
+    if (isGameOver()){
+        let gameOver = document.getElementById('game-over-lbl');
+        gameOver.innerHTML() = "Game Over"
+
     }
+
+        // let the lable with the id 'game-over-lbl' display the words 'Game Over' inside <h1> element
+    
 
     // I'll leave declaring the winner for your intrinsic motivation, it's not required for this assignment 
 }
